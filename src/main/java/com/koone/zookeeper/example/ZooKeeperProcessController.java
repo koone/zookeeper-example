c9d999512d895af3a54990e5a90341cac6e0d9ab
koone <luokun1028@126.com>
2017-02-24 19:24:16 +0800

    init

---
+++ src/main/java/com/koone/zookeeper/example/ZooKeeperProcessController.java
@@ -0,0 +1,14 @@
+/**
+ * Created by koone on 2017/2/24.
+ */
+package com.koone.zookeeper.example;
+
+/**
+ * a process controller, controll process start, stop ,change according to zookeeper znode
+ */
+public class ZooKeeperProcessController {
+
+
+}
+
+
